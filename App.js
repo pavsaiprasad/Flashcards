5a8fbe3ca3eb3e53400290735268b7c6cf0dac9c
pavsaiprasad <psaiprasad@asanov.home>
2018-05-28 17:36:49 +0100

    feat: added a tab navigator at the bottom for Home and Add New Deck

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import React from 'react'
+import { View, Platform, StatusBar } from 'react-native'
+import AddNewDeck from './components/AddNewDeck'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation'
 import { purple, white } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
+import Home from './components/Home'
 
 function Status({ backgroundColor, ...props }) {
   return (
@@ -14,11 +17,53 @@ function Status({ backgroundColor, ...props }) {
   )
 }
 
+const Tabs = createBottomTabNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    },
+  },
+  AddNewDeck: {
+    screen: AddNewDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add Deck',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    },
+  },
+}, {
+    navigationOptions: {
+      header: null
+    },
+    tabBarOptions: {
+      activeTintColor: Platform.OS === 'ios' ? purple : white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === 'ios' ? white : purple,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
+      }
+    }
+  })
+
+const MainNavigator = createStackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+})
+
 export default class App extends React.Component {
   render() {
     return (
         <View style={{ flex: 1 }}>
           <Status backgroundColor={purple} barStyle="light-content" />
+          <MainNavigator/>
         </View>
     )
   }
---
+++ components/AddNewDeck.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {View, Text} from 'react-native'
+
+class AddNewDeck extends Component {
+    render() {
+        return (
+            <View >
+                <Text>Add New Deck</Text>
+            </View>
+        )
+    }
+}
+
+export default AddNewDeck;
